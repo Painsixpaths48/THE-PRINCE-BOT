b5b6666b13ffd8c704ad0765685531b0db570060
killerbee48 <148557449+killerbee48@users.noreply.github.com>
2024-03-22 18:17:06 +0100

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`▢ Group : *${groupMetadata.subject}*\n▢ Members : *${participants.length}*${text ? `\n▢ Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` + users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ 𝙿𝚛𝚒𝚗𝚌𝚎 ┃ ᴮᴼᵀ ✪──', null, {
+    m.reply(`*【 اجــتماع الـجــروب ضد الكرنجات 】*\n\n❑ جروب : *${groupMetadata.subject}*\n❑ عدد الاعضاء : *${participants.length}*${text ? `\n❑ الرساله : ${text}\n` : ''}\n\n*〄━━┋ المختفون ┋━━〄*\n\n` + users.map(v => '*اجتمع┋* @' + v.replace(/@.+/, '')).join`\n` + '\n\n*【 بــــــ 𝑜𝒷𝒾𝓉𝑜 ــــــوت 】*', null, {
         mentions: users
     })
 }
 
 handler.help = ['tagall']
 handler.tags = ['group']
-handler.command = ['tagall']
+handler.command = ['منشن']
 handler.admin = true
 handler.group = true
 
