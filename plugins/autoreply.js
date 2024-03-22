782a7e9e1d22a13c6b2f01a82ba73758a38fc695
killerbee48 <148557449+killerbee48@users.noreply.github.com>
2024-03-22 18:23:27 +0100

    Update autoreply.js

--- plugins/autoreply.js
+++ plugins/autoreply.js
@@ -1,15 +1,89 @@
-export async function all(m) {
-	
-  // when someone sends a group link to the bot's dm
-  if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('open this link')) && !m.isBaileys && !m.isGroup) {
-   this.sendMessage(m.chat,{text:`Hello @${m.sender.split('@')[0]}\nyou can rent the bot to join a group\n\n_For more info you can DM the owner_\n*Type* \`\`\`.owner\`\`\` *to contact the owner*`.trim()}, {quoted:m});
-   /*this.sendButton(m.chat, `*Invite bot to a group*      
-    Hallo @${m.sender.split('@')[0]} 
-    you can rent the bot to join a group or contact owner 
-    more info click on the button
-  `.trim(), igfg, null, [['Rent', '/buyprem']] , m, { mentions: [m.sender] })*/
-   m.react(done)
-} 
-
- return !0
-}
+let handler = m => m 
+ handler.all = async function (m) { 
+ let chat = global.db.data.chats[m.chat] 
+  
+ if (/^احا$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `موت هاهااااااااا`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+  
+ if (/^الحمدلله$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, ` ادام الله حمدك `, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+   
+ if (/^بتعمل ايه دلوقت$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, ` بكلمك, `, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+  
+ if (/^انا جيت$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, ` ارجع من وين جيت هاهاااا `, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+  
+ if (/^هش$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `حاضر`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+  
+ if (/^حرامي|سارق$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `تتهم بريء بالسرقة 
+ من دون تحري او بحث 
+ عن حقيقة ليست ملموسة 
+ ارحنا واعمل شرطي  
+ ثم افتح فمك وثرثر 
+ فكلامك كـجاهل واحد  
+ بل جهلاً ارحم من حديثك 
+ تتصنع دور الشرطي  
+ وكأنك محقق 
+ بالله اصمت ولا تحرج نفسك  
+ ارحنا وارح أعصابك  
+ ان اكرمك الله بعقل 
+ فبسكوتك اقتل جهلك 
+ `, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+  
+  
+ if (/^ملل|مللل|ملللل$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, ` بسبب وجود حاتم الأمازيغي`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+  
+ if (/^السلام عليكم$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, ` وعليكم السلام `, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+  
+ if (/^تصبح علي خير|تصبحوا علي خير/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, ` وانت من اهل الخير ✨💜 `, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ }  
+  
+ if (/^هلا$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `من سمح للعبد بالتكلم`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+     
+ if (/^🐦$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, ` انظر بعيد `, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+  
+  if (/باي^$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `باي`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+     
+ if (/هلا^$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `أخرس يا زنجي `, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+  
+ if (/^كيفك|عامل ايه$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, ` الحمد لله و انت`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+ return !0 } 
+ export default handler
